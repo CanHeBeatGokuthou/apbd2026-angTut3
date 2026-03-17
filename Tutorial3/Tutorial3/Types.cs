aac8ee85efd8a19a85739f0115f123c0d429c73c
Kajetan Borkowski <kajetan.borkowski.com@gmail.com>
2026-03-17 09:11:35 +0100

    TypesOfUsersOnceAgain

--- Tutorial3/Tutorial3/Types.cs
+++ Tutorial3/Tutorial3/Types.cs
@@ -2,5 +2,18 @@
 
 public class Types
 {
-    
+    enum ItemType
+    {
+        Laptop,
+        Monitor,
+        Camera,
+        Projector,
+        Keyboard
+    }
+    enum UserType
+    {
+        Student,
+        Teacher,
+        Employee
+    }
 }
